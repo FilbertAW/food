49b2e109401939b42dd4202dcd0186ee2a5a5113
FilbertAW <filbertanthony01@gmail.com>
2018-05-20 16:34:15 +0700

    Initializing project

--- app/src/main/java/com/example/user/food/MainActivity.java
+++ app/src/main/java/com/example/user/food/MainActivity.java
@@ -7,15 +7,5 @@ import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ProgressBar bar = (ProgressBar)findViewById(R.id.simpleProgressBar2);
-        bar.getLayoutParams().width = 100;
-        bar.invalidate();
-        bar.setProgressTintList(ColorStateList.valueOf(Color.RED));
-    }
 
 }
