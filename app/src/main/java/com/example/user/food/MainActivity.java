78f1168bb22128668c607e82c276ed2d2fc5da5c
FilbertAW <filbertanthony01@gmail.com>
2018-06-01 03:39:25 +0700

    Initializing project

--- app/src/main/java/com/example/user/food/MainActivity.java
+++ app/src/main/java/com/example/user/food/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.user.food;
 import android.content.res.ColorStateList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
 
 
 }
