5af56299304ef24970e9fd69d981363712b949f3
FilbertAW <filbertanthony01@gmail.com>
2018-06-03 05:30:02 +0700

    notif2

--- app/src/main/java/com/example/user/food/MainActivity.java
+++ app/src/main/java/com/example/user/food/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.user.food;
 
-import android.content.res.ColorStateList;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.widget.ProgressBar;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.content.Context;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.example.user.food.R;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendNotification(View view) {
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this);
+
+        //Create the intent that’ll fire when the user taps the notification//
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.androidauthority.com/"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        mBuilder.setContentIntent(pendingIntent);
+
+        mBuilder.setSmallIcon(R.drawable.chocolate);
+        mBuilder.setContentTitle("Food Monitoring");
+        mBuilder.setContentText("Container 1 80% left");
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+            mBuilder.setVibrate(new long[] { 1000, 1000});
+            mNotificationManager.notify(001, mBuilder.build());
+
+    }
 
 
 }
